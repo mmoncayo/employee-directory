d9d845391ce947c3ff28bb5dff6e1a8062eeb4a0
Matt Moncayo <matthew.moncayo@gmail.com>
2020-08-15 18:58:20 -0700

    corrected API call to render properly

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-
 
 export default {
-    getUsers: function () {
-        return axios.get("https://randomuser.me/api/?results=200&nat=us")
+    async getEmployees() {
+        return fetch("https://randomuser.me/api/?results=200&nat=us")
+            .then((res) =>
+                res.json()
+            );
     }
 };
